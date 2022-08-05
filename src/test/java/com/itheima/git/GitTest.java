fd4adfa828d68b0c5412d22944d8465778751ce4
hhc <hhc@atguigu.com>
2022-08-05 16:31:06 +0800

    master commit2

--- src/test/java/com/itheima/git/GitTest.java
+++ src/test/java/com/itheima/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git 2222222222");
         System.out.println("hello hot-fix");
+        System.out.println("hello master2222222");
 
     }
 }
