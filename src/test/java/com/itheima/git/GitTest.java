cc9ae485b95bd18d2bc00859a10f67e169e009b7
hhc <hhc@atguigu.com>
2022-08-05 17:14:21 +0800

    push commit2

--- src/test/java/com/itheima/git/GitTest.java
+++ src/test/java/com/itheima/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hello master2222222");
         System.out.println("hello hot-fix111");
+        System.out.println("push hello");
 
     }
 }
